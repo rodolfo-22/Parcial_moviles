95a5c3f15d959bbc716d71b47d0ae1e14ee72b6f
rodolfo <00082421@uca.edu.sv>
2023-05-22 15:26:03 -0600

    adding_view

---
+++ app/src/main/java/com/example/hoteltracker_test/data/adapter/HotelAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.hoteltracker_test.data.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hoteltracker_test.data.model.HotelModel
+
+class HotelAdapter: RecyclerView.Adapter<HotelHolder> {
+    private var listhotel: List<HotelModel>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HotelHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: HotelHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/example/hoteltracker_test/data/adapter/HotelHolder.kt
@@ -0,0 +1,10 @@
+package com.example.hoteltracker_test.data.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.hoteltracker_test.databinding.FragmentBillBoardBinding
+
+class HotelHolder (private val binding: FragmentBillBoardBinding): RecyclerView.ViewHolder(binding.root) {
+    fun bind(){
+        binding.textVie
+    }
+}
